ab050b5ea8f01963a2edcd3f671b3524600385ad
Melih7342 <melih.gueldogdu@stud.fh-campuswien.ac.at>
2025-12-20 12:32:00 +0100

    scraper now gets all os_names from distrowatch

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,18 +1,34 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import time
 
-url = "https://en.wikipedia.org/wiki/List_of_Linux_distributions"
-headers = {'User-Agent': 'Mozilla/5.0'}
+# Browser-options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 
-response = requests.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
+# Start browser
+driver = webdriver.Chrome(options=chrome_options)
+
+url = "https://distrowatch.com/search.php?ostype=Linux&category=All&origin=All&basedon=All&notbasedon=None&desktop=All&architecture=All&package=All&rolling=All&isosize=All&netinstall=All&language=All&defaultinit=All&status=Active#simpleresults"
+driver.get(url)
+
+# Waiting time for scripts to load
+time.sleep(3)
+
+# Give the loaded html code to our soup
+soup = BeautifulSoup(driver.page_source, "html.parser")
+driver.quit()
 
 os_list = []
-for row in soup.select("table.wikitable tr"):
-    # Get first cell with the os name
-    cols = row.find_all("td")
-    if cols:
-        name = cols[0].get_text(strip=True)
+cells = soup.select("td.NewsText b a")
+for cell in cells:
+    name = cell.get_text(strip=True)
+    if name and not name.isdigit():
         os_list.append(name)
 
+os_list.remove("popularity")
+
+print(f"Found: {len(os_list)} systems")
 print(os_list)
