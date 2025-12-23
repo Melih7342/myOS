dc8c24831c30ba2a43c1ecfc67e2e7520850e706
MurtezaniAdis <adis.murtezani@stud.fh-campuswien.ac.at>
2025-12-23 14:55:35 +0100

    homepage almost done (only a little bit design needed) and quizpage functionality done, alot of design needed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Homepage from './Homepage.jsx'
 import Loginpage from './Loginpage.jsx'
 import Registerpage from './Registerpage.jsx'
-import Navbar from './Navbar.jsx'
+import Quizpage from './Quizpage.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Navbar />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Loginpage />} />
         <Route path="/register" element={<Registerpage />} />
+        <Route path="/quizpage" element={<Quizpage />} />
       </Routes>
     </Router>
   </StrictMode>,
